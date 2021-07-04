26706a233f3bea647886098a5eae5e4844351b2f
ryuken73 <ryuken73@gmail.com>
2021-07-04 16:17:48 +0900

    update: limit max level of map

--- src/KakaoMap.js
+++ src/KakaoMap.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const options = {
     center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3
+    level: 3,
+    mapTypeId : window.kakao.maps.MapTypeId.ROADMAP
 }
 
 function KakaoMap(props) {
-    const {setMap, setLocation, setLevel} = props;
+    const {setMap, setLocation, setLevel, maxLevel=13} = props;
     const container = React.useRef(null);
     React.useEffect(() => {
         const map = new window.kakao.maps.Map(container.current, options);
+        map.setMaxLevel(13)
         setMap(map);
         window.kakao.maps.event.addListener(map, 'click', function(mouseEvent) {  
             const latLng = mouseEvent.latLng;
